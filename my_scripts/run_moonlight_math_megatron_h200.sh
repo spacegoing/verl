22b565f61f755c5674a2dac3b048a902caf28c3d
spacegoing <spacegoing@gmail.com>
2025-10-20 13:46:36 +0000

    [refract & Fix] attn backend use fused explicitly

--- my_scripts/run_moonlight_math_megatron_h200.sh
+++ my_scripts/run_moonlight_math_megatron_h200.sh
@@ -24,13 +24,16 @@ RUNTIME_ENV=${RUNTIME_ENV:-"${HOME}/myCodeLab/host/verl/my_scripts/my_deepep_env
 
 # Export all wandb environment variables
 WANDB_DIR_NAME="wandb_my_dirs"
-WANDB_ROOT_DIR="$(pwd)/${WANDB_DIR_NAME}"
+WANDB_ROOT_DIR="/root/myCodeLab/host/verl/${WANDB_DIR_NAME}"
 mkdir -p "${WANDB_ROOT_DIR}"
-export WANDB_MODE="offline"
+export WANDB_MODE=offline
 export WANDB_DIR="${WANDB_ROOT_DIR}"
 export WANDB_CACHE_DIR="${WANDB_ROOT_DIR}/.cache"
 export WANDB_CONFIG_DIR="${WANDB_ROOT_DIR}/.config"
 export WANDB_DATA_DIR="${WANDB_ROOT_DIR}/wandb_data"
+# export NVTE_DEBUG=1
+# export NVTE_DEBUG_LEVEL=2
+# export NVTE_FUSED_ATTN=1
 
 adv_estimator=grpo
 
