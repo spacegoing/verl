c98ca96e7812cd1be637195449525be152294513
spacegoing <spacegoing@gmail.com>
2025-10-30 12:51:59 +0000

    [H800 Moonlight] Sharding updated

--- my_scripts/run_megatron_64node_h200.sh
+++ my_scripts/run_megatron_64node_h200.sh
@@ -80,7 +80,9 @@ offload=True
 optim_offload=False
 optimizer_offload_fraction=${OFFLOAD_FRACTION:-0.}
 
-gen_tp=16
+# VLLM / SGLANG
+gen_tp=32
+# Megatron
 train_tp=${TP:-2}
 train_pp=${PP:-8}
 EP=${EP:-8}
@@ -153,7 +155,7 @@ RAY_ADDRESS='auto' ray job submit --runtime-env="${RUNTIME_ENV}" -- \
     actor_rollout_ref.actor.entropy_coeff=0 \
     actor_rollout_ref.actor.optim.clip_grad=1.0 \
     actor_rollout_ref.actor.loss_agg_mode=${loss_agg_mode} \
-    actor_rollout_ref.rollout.gpu_memory_utilization=0.8 \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.6 \
     actor_rollout_ref.rollout.tensor_model_parallel_size=${gen_tp} \
     actor_rollout_ref.rollout.enable_chunked_prefill=True \
     actor_rollout_ref.rollout.max_num_batched_tokens=$((max_prompt_length + max_response_length)) \
